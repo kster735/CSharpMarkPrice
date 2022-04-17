f864440a46182b928f2f4681bdcba7d2e2b56e6f
kster007 <kster735@gmail.com>
2022-04-17 22:55:24 +0300

    My first .NET Core 6 Project on Linux!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Hello ");
+await Task.Delay(5000);
+Console.WriteLine("World!");
+
+int x;
+
+Console.Write("Δώσε έναν ακέραιο: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+int y;
+
+Console.Write("Δώσε άλλον έναν ακέραιο: ");
+y = Convert.ToInt32(Console.ReadLine());
+
+int sum = x + y;
+
+Console.WriteLine($"Το άθροισμα είναι: {sum}");
